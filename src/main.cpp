76fa3a4f624afd3b074a6fbc608699f782acee64
TD-er <gijs.noorlander@gmail.com>
2024-01-05 23:10:04 +0100

    [MCVE] Update to latest SDK for ESP8266/ESP32-xx
    Fix actually using the latest Arduino code

--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,36 @@
   #endif
 #endif
 
+#define COL 12
+#define ROW 4
+
+String str;
+float myfloat[COL][ROW];
+int idx;
+
+
 void setup() {
-
-  #ifdef USES_P001
-  #warning "USES_P001 defined"
-  #else
-    #warning "USES_P001 NOT defined"
-  #endif
-
+  idx = 0;
+  Serial.begin(115200);
+  for (int i = 0; i < COL; ++i) {
+    for (int j = 0; j < ROW; ++j) {
+      ++idx;
+      myfloat[i][j] = 1.0f * idx;
+    }
+  }
 }
 
 void loop() {
+  ++idx;
+  if (idx >= (COL*ROW)) {
+    idx = 0;
+  }
 
+  str = myfloat[idx / ROW][idx % ROW];
+  Serial.print(F("str: '"));
+  Serial.print(str);
+  Serial.print(F("' '"));
+  Serial.print(String(myfloat[idx / ROW][idx % ROW], 2));
+  Serial.println('\'');
+  delay(1000);
 }
