5f85798eedcbe98d2327de68c340cea1bd0fbca1
TD-er <gijs.noorlander@gmail.com>
2024-01-06 00:04:45 +0100

    softAPConfig fails with "DHCPS Set Netmask Failed! 0x5001" #9069
    See: https://github.com/espressif/arduino-esp32/issues/9069

--- src/main.cpp
+++ src/main.cpp
@@ -8,36 +8,27 @@
   #endif
 #endif
 
-#define COL 12
-#define ROW 4
 
-String str;
-float myfloat[COL][ROW];
-int idx;
-
+#include <IPAddress.h>
+#include <WiFiGeneric.h>
+#include <WiFi.h>
 
 void setup() {
-  idx = 0;
   Serial.begin(115200);
-  for (int i = 0; i < COL; ++i) {
-    for (int j = 0; j < ROW; ++j) {
-      ++idx;
-      myfloat[i][j] = 1.0f * idx;
-    }
-  }
+  Serial.setDebugOutput(true);
+
+
+  IPAddress apIP(192,168,4,1);
+  IPAddress gwIP(192,168,4,1);
+  IPAddress subnet(255,255,255,0);
+
+  WiFi.softAPConfig(apIP, gwIP, subnet);
+
+
+
+
 }
 
 void loop() {
-  ++idx;
-  if (idx >= (COL*ROW)) {
-    idx = 0;
-  }
-
-  str = myfloat[idx / ROW][idx % ROW];
-  Serial.print(F("str: '"));
-  Serial.print(str);
-  Serial.print(F("' '"));
-  Serial.print(String(myfloat[idx / ROW][idx % ROW], 2));
-  Serial.println('\'');
   delay(1000);
 }
