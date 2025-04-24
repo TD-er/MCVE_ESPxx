f4e9540d8dc789a93749395abde8cea711b35b43
TD-er <gijs.noorlander@gmail.com>
2025-04-24 22:05:08 +0200

    [IDF5.5] MCVE crash ESP32-classic

--- src/main.cpp
+++ src/main.cpp
@@ -11,29 +11,10 @@
   #endif
 #endif
 
-const uint8_t gpio_pins[]{
-  23,     22,     21,    19,    18,    5
-};
-
-constexpr size_t nrPins = sizeof(gpio_pins) / sizeof(gpio_pins[0]);
 
 
 void setup() {
   Serial.begin(115200);
-
-  for (size_t i = 0; i < nrPins; ++i) {
-    pinMode(gpio_pins[i], OUTPUT);
-  }
-
-  Wire.begin();
-  Wire1.begin();
-  Wire.end();
-
-
-  // Reset default SDA/SCL
-//  gpio_reset_pin(static_cast<gpio_num_t>(21));
-//  gpio_reset_pin(static_cast<gpio_num_t>(22));
-
 
   delay(100);
 
@@ -43,19 +24,9 @@ void setup() {
 void loop() {
   Serial.println("Start looping GPIO pins");
 
-  int counter = 0;
-  bool state = true;
 
   for (;;) {
-    for (size_t i = 0; i < nrPins; ++i) {
-//      pinMode(gpio_pins[i], OUTPUT);
-      digitalWrite(gpio_pins[i], state);
-      Serial.print("Set GPIO ");
-      Serial.print(gpio_pins[i]);
-      Serial.print(" to: ");
-      Serial.println(state ? 1 : 0);
-      delay(1000);    
-    }
-    state = !state;
+    delay(1000);
+    Serial.print('.');
   }
 }
