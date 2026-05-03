fbcf49e0f2ccaf98d98f1e37278bbc85927db1b4
TD-er <gijs.noorlander@gmail.com>
2026-05-03 16:28:02 +0200

    [ESP32S2] Start/stop USBSerial and Serial0

--- src/main.cpp
+++ src/main.cpp
@@ -11,34 +11,42 @@
 #include "WiFi.h"
 
 
-//#define SER Serial0
-#define SER USBSerial
-//#define SER_BEGIN  SER.begin(115200, SERIAL_8N1, 44, 43)
-#define SER_BEGIN  SER.begin(115200)
+#define SER Serial0
+#define SER1 USBSerial
+#define SER_BEGIN  SER.begin(115200, SERIAL_8N1, 44, 43)
+//#define SER_BEGIN  SER.begin(115200)
 
 
 void setup() {
+  SER1.begin();
   SER_BEGIN;
   SER.end();
+  SER1.end();
   delay(100);
+  SER1.begin();
   SER_BEGIN;
+}
+
+void process(Stream& s) {
+  if (s.available())  {
+    bool done = false;
+    s.println();
+    while (!done) {
+      int c = s.read();
+      if (c < 0) {
+        done = true;
+      } else {
+        s.print((char)c);
+      }
+    }
+    s.println();
+  }
+  s.write('.');
 }
 
 void loop()
 {
-  if (SER.available())  {
-    bool done = false;
-    SER.println();
-    while (!done) {
-      int c = SER.read();
-      if (c < 0) {
-        done = true;
-      } else {
-        SER.print((char)c);
-      }
-    }
-    SER.println();
-  }
-  SER.write('.');
+  process(SER);
+  process(SER1);
   delay(1000);
 }
